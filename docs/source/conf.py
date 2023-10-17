b3ba6f2111a3251621c348414dc80b1ee9b4f1cc
Magnus Johansson <magnus.johansson@u-blox.com>
2023-10-17 14:54:20 +0200

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'u-locate'
+copyright = '2023, u-blox'
+author = 'Magnus Johansson'
 
 release = '0.1'
 version = '0.1.0'
